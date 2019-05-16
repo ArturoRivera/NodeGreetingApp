5ec6001e6fbbb9dc39889cd8c8a6333e41f969ba
Arturo Rivera <a.rivera13@hotmail.com>
2019-05-15 22:50:45 -0700

    Basic Node App

---
+++ main.js
@@ -0,0 +1,17 @@
+const greetings = require("./greeting.js");
+const superheroes = require("./superheroes.js");
+
+// "Hello"
+greetings.greetingEnglish();
+// "Konichiwa"
+greetings.greetingJapanese();
+// "Hola"
+greetings.greetingSpanish();
+// "Hallo"
+greetings.greetingIcelandic();
+
+// "Tony Stark"
+superheroes.superIronMan();
+
+// "Iron Man"
+superheroes.superSpiderMan();
---
+++ superheroes.js
@@ -0,0 +1,7 @@
+exports.superIronMan = function(){
+  console.log("Tony Stark");
+}
+
+exports.superSpiderMan = function(){
+  console.log("Peter Parker");
+}
